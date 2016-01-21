be020e94dd6d919dd479ca119681a6ad0aee39e9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-01-21 14:18:13 +0100

    main script now includes metrocharts based off of two different datasets (one commented out, but convenient for testing)

--- src/ts/main-script.ts
+++ src/ts/main-script.ts
@@ -45,7 +45,27 @@ function doit(data: any) {
     spiral.render();
 
 
-    metrochart = new MetroChart('#metrochart', '/data/tintin-the-black-island.json');
+    let options: Options = {
+        enableTimeAxis: false,
+        charge: -5,
+        linkStrength: 1.0,
+        linkDistance: 30,
+        gravity: 0.0005,
+        colors: [
+            {name:'green', hexcode: '#008000'},
+            {name:'orange', hexcode: '#FF8000'},
+            {name:'red', hexcode: '#FF0000'},
+            {name:'yellow', hexcode: '#FFFF00'}
+        ],
+        stationShapeRadius: 5
+    };
+    metrochart = new MetroChart('#metrochart', '/data/metrolines-amsterdam.json', options);
+
+    // let options: Options = {
+    //     charge: 0,
+    //     linkStrength: 0.0
+    // };
+    // metrochart = new MetroChart('#metrochart', '/data/tintin-the-black-island.json', options);
 
 }
 
