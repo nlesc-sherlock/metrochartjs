92935400526c928844b55a385e1ff7a62e3fb9cb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-09-15 14:48:22 +0200

    added a main entrypoint for the module

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+/**
+ * MetroChart module
+ */
+
+
+export {MetroChart} from './metrochart';
+export {MetroChartData} from './metrochartdata';
+export {MetroChartError} from './metrocharterror';
+export {MetroLine} from './metroline';
+export {Options} from './options';
+export {Station} from './station';
+
